b32ee411d560e6260ef4b87435b6951ad1b14107
moraes-eduardo <moraes-eduardo@hotmail.com>
2023-02-01 16:13:06 -0300

    adicao exemplos

---
+++ src/candidatura/ProcessoSeletivoCase1.java
@@ -0,0 +1,21 @@
+public class ProcessoSeletivoCase1 {
+    public static void main(String[] args) throws Exception {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+
+    }
+
+    
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato.");
+        } else if (salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta.");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos.");
+        }
+    }
+}
